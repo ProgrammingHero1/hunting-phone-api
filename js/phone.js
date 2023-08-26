a6651f4cdc02070974be600da8f616c17d5f6570
Programming Hero <programming.hero1@gmail.com>
2023-08-26 13:39:53 -0500

    show phone details on a modal

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) =>{
+const loadPhone = async (searchText='13', isShowAll) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
@@ -8,7 +8,7 @@ const loadPhone = async (searchText, isShowAll) =>{
 
 
 const displayPhones = (phones, isShowAll) =>{
-    console.log(phones)
+    // console.log(phones)
 
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container cards before adding new cards
@@ -22,14 +22,14 @@ const displayPhones = (phones, isShowAll) =>{
     else{
         showAllContainer.classList.add('hidden');
     }
-    console.log('is show all', isShowAll)
+    // console.log('is show all', isShowAll)
     // display only first 12 phones if not show All
     if(!isShowAll){
         phones = phones.slice(0,12);
     }
 
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
         // 2 create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
@@ -54,11 +54,31 @@ const displayPhones = (phones, isShowAll) =>{
 
 // 
 const handleShowDetail = async (id) =>{
-    console.log('clicked show details', id)
+    // console.log('clicked show details', id)
     // load single phone data
     const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
     const data = await res.json();
-    console.log(data);
+    const phone = data.data;
+    
+    showPhoneDetails(phone)
+
+}
+
+const showPhoneDetails = (phone) =>{
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+
+    showDetailContainer.innerHTML = `
+        <img src="${phone.image}" alt="" />
+        <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+        <p><span>GPS:</span>${phone?.others?.GPS || 'No GPS'}</p>
+    `
+
+    // show the modal
+    show_details_modal.showModal();
 }
 
 
@@ -93,4 +113,4 @@ const handleShowAll = () =>{
     handleSearch(true);
 }
 
-// loadPhone();
+loadPhone();
