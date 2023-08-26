9ebfca619b5a6dedc15a8cd646de333ef9091473
Programming Hero <programming.hero1@gmail.com>
2023-08-25 21:55:07 -0500

    conditional show all button

--- js/phone.js
+++ js/phone.js
@@ -8,14 +8,26 @@ const loadPhone = async (searchText) =>{
 
 
 const displayPhones = phones =>{
-    // console.log(phones)
+    console.log(phones)
 
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container cards before adding new cards
     phoneContainer.textContent = '';
 
+    // display show all button if there are more than 12 phones
+    const showAllContainer = document.getElementById('show-all-container')
+    if(phones.length > 12){
+        showAllContainer.classList.remove('hidden')
+    }
+    else{
+        showAllContainer.classList.add('hidden');
+    }
+
+    // display only first 12 phones
+    phones = phones.slice(0,12);
+
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
         // 2 create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
@@ -41,6 +53,12 @@ const handleSearch = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
+    loadPhone(searchText);
+}
+
+const handleSearch2 = () =>{
+    const searchField = document.getElementById('search-field2')
+    const searchText = searchField.value;
     loadPhone(searchText);
 }
 
