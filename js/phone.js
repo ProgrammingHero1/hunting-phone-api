8e0423ef4804df82dd0e455706c146d2100c3bdf
Programming Hero <programming.hero1@gmail.com>
2023-08-25 20:25:32 -0500

    implemented search phone

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -11,12 +11,14 @@ const displayPhones = phones =>{
     // console.log(phones)
 
     const phoneContainer = document.getElementById('phone-container');
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
 
     phones.forEach(phone =>{
         console.log(phone);
         // 2 create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
         // 3: set inner html
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -34,4 +36,12 @@ const displayPhones = phones =>{
 }
 
 
-loadPhone();
+// handle search button
+const handleSearch = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
+// loadPhone();
